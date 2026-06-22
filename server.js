fc51468dea3c8d8652db771adcb26575268b6279
Murat <sfa07092017@gmail.com>
2026-06-22 18:49:23 +0300

    fix CORS allow all origins

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // In-memory store for latest readings
